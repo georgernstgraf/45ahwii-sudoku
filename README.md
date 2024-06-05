faec3c69f1be50de08b2a4070a5394318e12a88e
Georg Graf (SPG) <grafg@spengergasse.at>
2024-06-05 14:01:14 +0200

    Start

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # 45ahwii-sudoku
 
+## Aufgaben Teilung
+
+### Frontend
+
+- Ben
+- Jasim
+
+### Alg. Amin / Andreas
+
+- Amin
+- Andreas
+
+### Alg. Massimo
+
+- Massimo
+- Emil
+
+### Rekursion
+
+- Jakob
+- Matthias
+
---
+++ sudoku.js
@@ -0,0 +1,22 @@
+class Sudoku {
+    constructor(jsn) {
+        this._data = jsn;
+    }
+
+    obvious() {
+        // so lange obviuos felder befüllen bis sich nix mehr tut
+        // a) massimo
+        // b) amin / andreas
+        return true; // ob sich was geändert hat oder nicht
+    }
+
+    annahmen() {
+        return; // {c1: [1,2,4]}
+    }
+
+    // probiere für jede Annahme das Sudoku neu zu lösen
+
+    loese() {
+        return; // erste Lösung oder null
+    }
+}
