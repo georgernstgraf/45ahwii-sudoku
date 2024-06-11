c552caa8a2361fcf49a6149a5b4a391a67af1648
# <jakob.baumgartner@gmail.com>
2024-06-11 20:43:05 +0200

    Txt zu json converter

--- converter.js
+++ converter.js
@@ -1,7 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
 function sudoku2js(fileName) {
-    // read file
-    // convert it to an object
-    const obj = {};
+    let obj = {};
+    fs.readFile(fileName, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Fehler beim Lesen der Datei:', err);
+            return;
+        }
+
+        const lines = data.split('\n');
+        const jsonObject = {};
+
+        lines.forEach(line => {
+            const [key, value] = line.split(':').map(item => item.trim());
+            if (key && value) {
+                jsonObject[key] = value;
+            }
+        });
+        obj = JSON.parse(JSON.stringify(jsonObject))
+    });
     return obj;
 }
+console.log(sudoku2js(__dirname + "\\input.txt"))
 module.exports = sudoku2js;
