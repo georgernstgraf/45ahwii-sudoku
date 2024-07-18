54b8b6545d6a6682dd8057903a5b6bd390904c87
GREyklexx <GRE22432@spengergasse.at>
2024-07-18 11:06:29 +0200

    FESCRIPT liest nun UnsolvedSudoku1.json aus und zeigt es an!

--- JsonReadTest.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-// Read the JSON file
-const jsonData = fs.readFileSync('/SUDOKUS/unsolvedSudoku1.json', 'utf-8');
-
-// Parse the JSON data
-const data = JSON.parse(jsonData);
-
-// Use the data as needed
-console.log(data);
