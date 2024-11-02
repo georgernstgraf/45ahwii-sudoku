72b34e42b1ada972d58794edf94e76fdb3a8e3a5
Georg Graf (SPG) <grafg@spengergasse.at>
2024-11-02 19:21:48 +0100

    it solves but it is ugly

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,152 +1,186 @@
-import { Sudoku, Cell, colNames, rowNames, Grid } from './sudoku.js';
-// 1: State
-const state = { maxSolutions: 1 };
-// 2: State access and mod
-function resetState() {
-    Object.assign(state, {
-        sudoku: null,
-        foundSolutions: [],
-        recursionDepth: 0,
-        allSteps: [],
-        intervalId: NaN,
-        error: "OK"
-    });
-}
-function initGrid() {
-    //Matthias war hier
-    state.sudoku = null;
-    const grid = sudokuGrid$;
-    Array.from(sudokuGrid$.children).forEach(c => c.remove());
-    for (let i = 1; i <= 9; i++) {
-        const subgrid = document.createElement("div");
-        subgrid.classList.add("subgrid");
-        subgrid.classList.add("border");
-        subgrid.classList.add("border-2");
-        subgrid.classList.add("rounded-2");
-        subgrid.id = "subgrid-" + i;
-        for (let key of getSubGridKeys(i)) {
-            const cell = document.createElement("div");
-            cell.classList.add("grid-item");
-            cell.classList.add("border");
-            cell.id = key;
-            cell.textContent = key;
-            subgrid.appendChild(cell);
-            // console.log(key);
-        }
-        grid.appendChild(subgrid);
+import { Sudoku } from './sudoku.js';
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    // 1: State
+    const state = {
+        maxSolutions: 1,
+    };
+    // 2: State access and mod
+    function resetState() {
+        Object.assign(state, {
+            sudoku: null,
+            original: null,
+            foundSolutions: [],
+            recursionDepth: 0,
+            allSteps: [],
+            intervalId: NaN,
+            error: "OK"
+        });
     }
-}
-function getSubGridKeys(num) {
-    const col = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
-    const row = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    num = num - 1;
-    let firstColIndex = (num % 3) * 3;
-    let firstRowIndex = Math.floor(num / 3) * 3;
-    let keys = [];
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            keys.push(col[firstColIndex + j] + row[firstRowIndex + i]);
+    function initGrid() {
+        //Matthias war hier
+        state.sudoku = null;
+        const grid = sudokuGrid$;
+        Array.from(sudokuGrid$.children).forEach(c => c.remove());
+        for (let i = 1; i <= 9; i++) {
+            const subgrid = document.createElement("div");
+            subgrid.classList.add("subgrid");
+            subgrid.classList.add("border");
+            subgrid.classList.add("border-2");
+            subgrid.classList.add("rounded-2");
+            subgrid.id = "subgrid-" + i;
+            for (let key of getSubGridKeys(i)) {
+                const cell = document.createElement("div");
+                cell.classList.add("grid-item");
+                cell.classList.add("border");
+                cell.id = key;
+                cell.textContent = key;
+                subgrid.appendChild(cell);
+                // console.log(key);
+            }
+            grid.appendChild(subgrid);
         }
     }
-    // console.log(keys);
-    return keys;
-}
-async function fetchSudoku(url) {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(response.statusText);
+    function getSubGridKeys(num) {
+        const col = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+        const row = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        num = num - 1;
+        let firstColIndex = (num % 3) * 3;
+        let firstRowIndex = Math.floor(num / 3) * 3;
+        let keys = [];
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                keys.push(col[firstColIndex + j] + row[firstRowIndex + i]);
+            }
+        }
+        // console.log(keys);
+        return keys;
     }
-    const obj = await response.json();
-    resetState();
-    const sudoku = new Sudoku({ data: obj, recursionDepth: 0, state });
-    state.sudoku = sudoku;
-}
+    async function fetchSudoku(url) {
+        resetState();
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        const obj = await response.json();
+        const sudoku = new Sudoku({ data: obj, recursionDepth: 0, state });
+        state.sudoku = sudoku;
+        state.original = new Sudoku(sudoku);
+    }
 
-// 3 - dom node refs
-const error$ = document.getElementById("error");
-const sudokuGrid$ = document.getElementById("sudoku-container");
-const beispieleSelect$ = document.getElementById("beispieleSelect");
-const maxSolutionsIn$ = document.getElementById("max-solves-in");
-const maxSolutionsOut$ = document.getElementById("max-solves-out");
-const solvesCountOut$ = document.getElementById("solves-count-out");
-const startSolving$ = document.getElementById("start-solving");
-// 4 - dom node creation
-// 5 -render functions
-function render() {
-    // den gesamten state in den div.
-    // elementen anzeigen
-    console.log('render called');
-    if (state.sudoku) {
+    // 3 - dom node refs
+    const error$ = document.getElementById("error");
+    const sudokuGrid$ = document.getElementById("sudoku-container");
+    state.domNode = sudokuGrid$;
+    const beispieleSelect$ = document.getElementById("beispieleSelect");
+    const maxSolutionsIn$ = document.getElementById("max-solves-in");
+    const maxSolutionsOut$ = document.getElementById("max-solves-out");
+    const solvesCountOut$ = document.getElementById("solves-count-out");
+    const startSolving$ = document.getElementById("start-solving");
+    const stopSolving$ = document.getElementById("stop-solving");
+    // 4 - dom node creation
+    // 5 -render functions
+    function render() {
+        // den gesamten state in den div.
+        // elementen anzeigen
+        if (state.sudoku) {
+            try {
+                state.sudoku.render(sudokuGrid$);
+            }
+            catch (e) {
+                state.error = e.message;
+                console.error(e);
+            }
+        }
+        error$.innerText = state.error;
+        if (state.error == "OK") {
+            error$.classList.remove("bg-danger");
+        } else {
+            error$.classList.add("bg-danger");
+        }
+        maxSolutionsOut$.innerText = state.maxSolutions;
+        solvesCountOut$.innerText = state.foundSolutions.length;
+    };
+
+    function displaySudoku(sudoku) {
+        sudoku.render(sudokuGrid$);
+    }
+    function displayNextStep() {
+        const sudoku = state.allSteps.shift();
+        displaySudoku(sudoku);
+    }
+    // 6 Event Handlers
+    async function onSelectSudoku() {
+        const url = beispieleSelect$.value;
+        console.log("start fetching: ", url);
+        state.error = "now fetching";
+        render();
         try {
-            state.sudoku.renderInto(sudokuGrid$);
+            await fetchSudoku(url);
+            state.error = "OK";
         }
         catch (e) {
             state.error = e.message;
             console.error(e);
-        }
-    }
-    error$.innerText = state.error;
-    if (state.error == "OK") {
-        error$.classList.remove("bg-danger");
-    } else {
-        error$.classList.add("bg-danger");
-    }
-    maxSolutionsOut$.innerText = state.maxSolutions;
-
-};
-
-function displaySudoku(sudoku) {
-    sudoku.renderInto(sudokuGrid$);
-}
-function displayNextStep() {
-    const sudoku = state.allSteps.shift();
-    displaySudoku(sudoku);
-}
-// 6 Event Handlers
-async function onSelectSudoku() {
-    const url = beispieleSelect$.value;
-    console.log("start fetching: ", url);
-    state.error = "now fetching";
-    render();
-    try {
-        await fetchSudoku(url);
-        state.error = "OK";
-    }
-    catch (e) {
-        state.error = e.message;
-        console.error(e);
-    };
-    render();
-}
-function onMaxSolutionsSlider() {
-    state.maxSolutions = maxSolutionsIn$.value;
-    render();
-}
-function onStartSolving() {
-    if (!state.sudoku) {
-        state.error = "No Sudoku loaded";
+        };
         render();
-        return;
     }
-    state.error = "Now solving ..";
-    try {
+    function onMaxSolutionsSlider() {
+        state.maxSolutions = maxSolutionsIn$.value;
+        render();
+    }
+    function onStopSolving() {
+        clearInterval(state.interval);
+        state.interval = undefined;
+    }
+    function onStartSolving() {
+        if (!state.sudoku) {
+            state.error = "No Sudoku loaded";
+            render();
+            return;
+        }
+        state.error = "Now solving ..";
         state.sudoku.solve();
-        state.error = "OK";
-    } catch (e) {
-        state.error = e.message;
+        // state.error ="OK";
+        state.interval = setInterval(nextStep, 200);
     }
-    render();
-}
-// 7 - init bindings
-maxSolutionsIn$.onchange = onMaxSolutionsSlider;
-startSolving$.onclick = onStartSolving;
-beispieleSelect$.onchange = onSelectSudoku;
-
-// 8 - initial render
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
+    function nextStep() {
+        let obj = state.allSteps.pop();
+        if (!obj) obj = [undefined, undefined];
+        const [sud, func] = obj;
+        if (state.foundSolutions.length >= state.maxSolutions || !sud || !func) {
+            if (state.foundSolutions.length >= state.maxSolutions) {
+                console.info("Stop. Enough solutions");
+            }
+            if (!sud) {
+                console.info("Stop. No sud");
+            }
+            if (!func) {
+                console.info("Stop. No func");
+            }
+            onStopSolving();
+            state.sudoku = state.foundSolutions[0];
+            state.error = "OK";
+            render();
+            return;
+        }
+        func();
+        state.sudoku = sud;
+        render();
+    }
+    // 7 - init bindings
+    maxSolutionsIn$.onchange = onMaxSolutionsSlider;
+    stopSolving$.onclick = onStopSolving;
+    startSolving$.onclick = onStartSolving;
+    beispieleSelect$.onchange = onSelectSudoku;
+    const self = this;
+    // 8 - initial render
     // Your initialization code here
     initGrid();
     resetState();
+    maxSolutionsIn$.value = state.maxSolutions;
+    window.gg = {
+        "s": state, "d": displaySudoku,
+    };
     render();
 });
--- frontend/js/sudoku.js
+++ frontend/js/sudoku.js
@@ -1,10 +1,7 @@
+import { assert } from "./chai.js";
 const colNames = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
-const rowNames = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const rowNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 class Sudoku {
-    #recursion_depth;
-    _maxSolutions;
-    _data;
-    _foundSolutions;
     constructor(obj) {
         if (obj instanceof Sudoku) {
             // do the deep copies here
@@ -19,91 +16,64 @@ class Sudoku {
         this.state = state;
     }
 
-    solveObvious() {
-        // so lange obviuos felder befüllen bis sich nix mehr tut
-        // a) massimo  // TODO fehler in schleife
-        const countInital = this.grid.countSetCells();
-        let countBefore = countInital;
-        while (true) {
-            doMassimos(); // grid befüllen mit new Cell()'s
-            const countAfter = this.grid.countSetCells();
-            if (countBefore === countAfter) break; // nix mehr zu holen
-            countBefore = countAfter;
-        }
-        const massimos = this.grid.countSetCells() - countBefore;
-        if (massimos > 0) {
-            console.log(
-                `Massimo found ${massimos} new values / depth: ${this.#recursion_depth
-                }`
-            );
-        }
-        // b) amin / andreas
-        countBefore = this.grid.countSetCells();
-        while (true) {
-            doAminAndreas(); // grid befüllen mit new Cell()'s
-            const countAfter = this.grid.countSetCells();
-            if (countBefore === countAfter) break; // nix mehr zu holen
-            countBefore = countAfter;
-        }
-        const aminadreas = this.grid.countSetCells() - countBefore;
-        if (aminadreas > 0) {
-            console.log(
-                `Amin / Andreas found ${aminadreas} new values / depth: ${this.#recursion_depth
-                }`
-            );
-        }
-        return this.grid.countSetCells() - countInital;
+    storeSolution() {
+        this.state.foundSolutions.push(new Sudoku(this)); // store the copy
     }
-    makeAssumptions() {
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_generators
-        // iterator bauen, der neue Sudoku Objekte liefert!!
-        for (sudoku of this.assumptions()) {
-            sudoku.solve();
-            if (this.state.foundSolutions <= this.state.maxSolutions) break;
-        }
-        return;
+    storeStep(func) {
+        this.state.allSteps.push([new Sudoku(this), func]); // store the copy
     }
-    // probiere für jede Annahme das Sudoku neu zu lösen
     solve() {
+        this.do_obvious();
         if (this.grid.isFull()) {
-            if (!this.grid.isValid()) {
-                throw new Error("PANIC: Grid is full but invalid");
-            }
-            this.state.foundSolutions.push(this.deepCopy());
+            this.storeSolution();
             return;
         }
-        if (this.grid.isFull()) {
-            console.error(
-                "ERROR: Sudoku is already solved, but you called solve()."
-            );
-            return;
-        }
-        const newCellCount = this.solveObvious();
-        if (newCellCount > 0) {
-            console.log(
-                `obvious found ${newCellCount} new values at depth ${this.#recursion_depth
-                }`
-            );
-        }
-        this.makeAssumptions();
+        this.storeStep(this.do_solve.bind(this));
     }
-    renderInto(domNode) {
+    do_obvious() {
+        const obviousCount = this.grid.fillObvious();
+        if (!this.grid.isValid()) {
+            throw new Error("Grid is not valid");
+        }
+        // if we have enough solutions: stop
+        // store a found Solution in the state
+        //if (obviousCount) {
+        //    console.log(`Did ${obviousCount} obvious placements`);
+        //}
+        // valid
+        // not full
+        // everything obvious done, recursion needed
+        //this.grid.setAssumptions(); only for fancy display
+    }
+    do_solve() {
+        const assumption = this.grid.getAssumption();
+        const position = Object.keys(assumption)[0];
+        for (let value of assumption[position].toReversed()) {
+            const sudoku = new Sudoku(this);
+            sudoku.recursionDepth++;
+            console.info(`Depth: ${sudoku.recursionDepth} for "${position}" I try "${value}"`);
+            sudoku.grid.setCell(position, new Cell({ value, position }));
+            sudoku.solve();
+        }
+    }
+
+    render(domNode = this.state.domNode) {
         Array.from(domNode.querySelectorAll(".grid-item")).forEach(
             (e) => (e.innerHTML = "")
         );
         for (let pos in this.grid.data) {
             const elt = domNode.querySelector(`#${pos}`);
-            elt.innerText = this.grid.data[pos].value;
-            // elt.classList = "recursionDepth-" + this.recursionDepth; TODO data-recursion=3
+            const val = this.grid.data[pos].value;
+            if (val)
+                elt.innerText = val;
+            // elt.classList = "recursionDepth-" + this.recursionDepth; TODO-evtl data-recursion=3
         }
     }
 }
 class Cell {
+    value = false;
     constructor(par) {
         Object.assign(this, par);
-        if (this.isAssumption == undefined) {
-            this.isAssumption = false;
-        }
     }
 }
 class Grid {
@@ -113,7 +83,7 @@ class Grid {
             // need to return a copy
             this.recursionDepth = par.recursionDepth;
             this.data = {};
-            Object.keys(par.data).forEach(position => { this.data[position] = new Cell(par.data[position]); });
+            Object.keys(par.data).forEach(position => { if (par.data[position].value) { this.data[position] = new Cell(par.data[position]); } });
             return;
         }
         const { recursionDepth, data } = par;
@@ -123,7 +93,7 @@ class Grid {
         this.recursionDepth = recursionDepth;
         const newData = {};
         for (let position in par.data) {
-            if (typeof position != "string" || position.length != 2 || !colNames.includes(position[0]) || !rowNames.includes(position[1])) {
+            if (typeof position != "string" || position.length != 2 || !colNames.includes(position[0]) || !rowNumbers.includes(position[1])) {
                 throw new Error(`Position "${position}" is not valid`);
             }
             let value = par.data[position];
@@ -137,28 +107,72 @@ class Grid {
             if (isNaN(value) || value < 1 || value > 9) {
                 throw new Error(`Number "${value}" at Position "${position}" is not valid`);
             }
-            newData[position] = new Cell({ position, value, recursionDepth, isAssumption: false });
+            newData[position] = new Cell({ position, value, recursionDepth });
         }
         this.data = newData;
         if (!this.isValid()) {
             throw new Error("Invalid grid");
         }
     }
-
     setCell(position, value) {
         if (!(value instanceof Cell)) {
             throw new Error("Value must be an instance of Cell");
         }
         this.data[position] = value;
     }
+    // use this only for fancy display
+    //setAssumptions() {
+    //    if (!this.data[key]) {
+    //        this.data[key] = new Cell({ recursionDepth: this.recursionDepth, position: key });
+    //    }
+    //    this.data[key].possibilities = possibilities;
+    //}
+    getAssumption() {
+        const position = this.allEmptyCellNames()[0];
+        const possibilities = this.getPossibleFor(position);
+        const rv = {};
+        rv[position] = possibilities;
+        return rv;
+    }
+    getPossibleFor(key) {
+        const rv = [];
+        const col = key[0];
+        const row = key[1];
+        const subSquare = Grid.getSubGridFromPos(key);
+        for (let val = 1; val <= 9; val++) {
+            if (!this.isRowValid(row, val)) continue;
+            if (!this.isColValid(col, val)) continue;
+            if (!this.isSquareValid(subSquare, val)) continue;
+            rv.push(val);
+        }
+        return rv;
+    }
+    fillObvious() {
+        let doAgain;
+        let foundCount = 0;
+        do {
+            doAgain = false;
+            for (let position of this.allEmptyCellNames()) {
+                const possible = this.getPossibleFor(position);
+                if (possible.length == 1) {
+                    const value = possible[0];
+                    this.data[position] = new Cell({ value, position });
+                    //  console.log(`found obvious "${value}" at "${position}"`);
+                    doAgain = true;
+                    foundCount++;
+                    break;
+                }
+            }
+        } while (doAgain);
+        return foundCount;
+    }
     allEmptyCellNames() {
         const keys = [];
         for (let col of colNames) {
-            for (let row of rowNames) {
-                const key = `${col}${row}`;
-                if (!(key in this.data)) {
+            for (let row of rowNumbers) {
+                const key = col + row;
+                if (!this.data[key]?.value)
                     keys.push(key);
-                }
             }
         }
         return keys;
@@ -166,80 +180,53 @@ class Grid {
     isFull() {
         return this.allEmptyCellNames().length === 0;
     }
-    countSetCells() {
-        let count = 0;
-        Object.keys(this.data).forEach(k => {
-            count += this.data[k]?.value ? 1 : 0;
-        });
-        return count;
-    }
-
-    isValidRows() {
-        for (let rowName of rowNames) {
-            if (this.isValidRow(rowName) === false) return false;
+    areAllRowsValid() {
+        for (let rowNum of rowNumbers) {
+            if (!this.isRowValid(rowNum)) return false;
         }
         return true;
     }
-    isValidRow(rowName) {
-        const set = new Set();
+    areAllColsValid() {
         for (let colName of colNames) {
-            const cell = colName + rowName;
-            if (this.data[cell] == undefined) continue;
-
-            if (set.has(this.data[cell])) return false;
-            set.add(this.data[cell]);
+            if (!this.isColValid(colName)) return false;
         }
         return true;
     }
-
-    isValidCols() {
-        for (let colName of colNames) {
-            if (this.isValidCol(colName) == false) return false;
-        }
-        return true;
-    }
-    isValidCol(colName) {
-        const set = new Set();
-        for (let rowName of rowNames) {
-            const cell = colName + rowName;
-            if (this.data[cell] == undefined) continue;
-            if (set.has(this.data[cell])) {
-                console.log("col " + colName + " is invalid");
-                return false;
-            }
-            set.add(this.data[cell]);
-            // console.log(set);
-        }
-        return true;
-    }
-
-    isValidSquares() {
+    areSquaresValid() {
         for (let i = 1; i <= 9; i++) {
-            if (this.isValidSquare(i) === false) return false;
+            if (!this.isSquareValid(i)) return false;
         }
         return true;
     }
-
-    isValidSquare(num) {
-        const keys = Grid.getSubGridKeys(num);
-        const values = new Set();
-        for (let k of keys) {
-            const value = this.data[k]?.value;
-            if (!value) {
-                return;
-            }
-            if (values.has(value)) {
-                return false;
-            }
-            values.add(value);
+    isRowValid(rowNumber, val) {
+        const set = new Set();
+        if (val) { set.add(val); }
+        for (let colName of colNames) {
+            const cell = colName + rowNumber;
+            if (!this.data[cell]?.value) continue;
+            if (set.has(this.data[cell].value)) return false;
+            set.add(this.data[cell].value);
         }
         return true;
-
-        for (let i = 0; i < keys.length; i++) {
-            const cell = keys[j];
-            if (this.data[cell] == undefined) continue;
-            if (values.has(this.data[cell])) return false;
-            values.add(this.data[cell]);
+    }
+    isColValid(colName, val) {
+        const set = new Set();
+        if (val) { set.add(val); }
+        for (let rowNumber of rowNumbers) {
+            const cell = colName + rowNumber;
+            if (!this.data[cell]?.value) continue;
+            if (set.has(this.data[cell].value)) return false;
+            set.add(this.data[cell].value);
+        }
+        return true;
+    }
+    isSquareValid(num, val) {
+        const set = new Set();
+        if (val) { set.add(val); }
+        for (let cell of Grid.getSubGridKeys(num)) {
+            if (!this.data[cell]?.value) continue;
+            if (set.has(this.data[cell].value)) return false;
+            set.add(this.data[cell].value);
         }
         return true;
     }
@@ -250,28 +237,39 @@ class Grid {
         const keys = [];
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                keys.push(colNames[firstColIndex + j] + rowNames[firstRowIndex + i]);
+                keys.push(colNames[firstColIndex + j] + rowNumbers[firstRowIndex + i]);
             }
         }
         return keys;
     }
-
+    static getSubGridFromPos(pos) {
+        const colChar = pos[0];
+        const rowChar = pos[1];
+        let col = colNames.indexOf(colChar);
+        let row = rowNumbers.indexOf(rowChar);
+        assert(col >= 0 && col < 9, `pos ${pos} yields invalid col ${col}`);
+        assert(row >= 0 && row < 9, `pos ${pos} yields invalid col ${row}`);
+        row = Math.floor(row / 3);
+        col = Math.floor(col / 3);
+        const rv = 1 + row * 3 + col;
+        assert(rv >= 1 && rv <= 9, `pos ${pos} yields invalid subgrid ${rv}`);
+        return rv;
+    }
     isValid() {
-        if (!this.isValidRows()) {
+        if (!this.areAllRowsValid()) {
             console.log("Rows are invalid");
-
             return false;
         }
-        if (!this.isValidCols()) {
+        if (!this.areAllColsValid()) {
             console.log("Cols are invalid");
             return false;
         }
-        if (!this.isValidSquares()) {
+        if (!this.areSquaresValid()) {
             console.log("Squares are invalid");
             return false;
         }
-        console.log("Grid is valid");
+        //console.log("Grid is valid");
         return true;
     }
 }
-export { Sudoku, Cell, colNames, rowNames, Grid };
+export { Sudoku, Cell, colNames, rowNumbers, Grid };
